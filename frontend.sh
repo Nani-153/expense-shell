d037ce577d0900c60960883eaf57ead499537234
Nani-153 <naninarayanag088@gmail.com>
2025-07-12 17:37:09 +0530

    adding expense

--- frontend.sh
+++ frontend.sh
@@ -1,62 +1,124 @@
 #!/bin/bash
 
-LOGS_FOLDER="/var/log/expense-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
+# LOGS_FOLDER="/var/log/expense-logs"
+# SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+# TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+# LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+# mkdir -p $LOGS_FOLDER
+
+# USERID=$(id -u)
+# R="\e[31m"
+# G="\e[32m"
+# N="\e[0m"
+# Y="\e[33m"
+
+# CHECK_ROOT(){
+#     if [ $USERID -ne 0 ]
+#     then
+#         echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+#         exit 1
+#     fi
+# }
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then
+#         echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+#         exit 1
+#     else
+#         echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+#     fi
+# }
+
+# echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# CHECK_ROOT
+
+# dnf install nginx -y &>>$LOG_FILE
+# VALIDATE $? "Installing Nginx"
+
+# systemctl enable nginx &>>$LOG_FILE
+# VALIDATE $? "Enable Nginx"
+
+# systemctl start nginx &>>$LOG_FILE
+# VALIDATE $? "Start Nginx"
+
+# rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+# VALIDATE $? "Removing default website"
+
+# curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+# VALIDATE $? "Downloding frontend code"
+
+# cd /usr/share/nginx/html
+# unzip /tmp/frontend.zip &>>$LOG_FILE
+# VALIDATE $? "Extract frontend code"
+
+# cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+# VALIDATE $? "Copied expense conf"
+
+# systemctl restart nginx &>>$LOG_FILE
+# VALIDATE $? "Restarted Nginx"
+
+
+#!/bin/bash
 
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-N="\e[0m"
 Y="\e[33m"
+N="\e[0m"
 
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+LOGS_FOLDER="/var/log/expense-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "ERROR:: You must have sudo access to execute this script"
+        exit 1 #other than 0
+    fi
+}
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
 CHECK_ROOT
 
-dnf install nginx -y &>>$LOG_FILE
-VALIDATE $? "Installing Nginx"
+dnf install nginx -y  &>>$LOG_FILE_NAME
+VALIDATE $? "Installing Nginx Server"
 
-systemctl enable nginx &>>$LOG_FILE
-VALIDATE $? "Enable Nginx"
+systemctl enable nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Enabling Nginx server"
 
-systemctl start nginx &>>$LOG_FILE
-VALIDATE $? "Start Nginx"
+systemctl start nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Starting Nginx Server"
 
-rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
-VALIDATE $? "Removing default website"
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE_NAME
+VALIDATE $? "Removing existing version of code"
 
-curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
-VALIDATE $? "Downloding frontend code"
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE_NAME
+VALIDATE $? "Downloading Latest code"
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "Extract frontend code"
+VALIDATE $? "Moving to HTML directory"
 
-cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
-VALIDATE $? "Copied expense conf"
+unzip /tmp/frontend.zip &>>$LOG_FILE_NAME
+VALIDATE $? "unzipping the frontend code"
 
-systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "Restarted Nginx"
+cp /home/ec2-user/shell-expense/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expense config"
 
-
+systemctl restart nginx &>>$LOG_FILE_NAME
+VALIDATE $? "Restarting nginx"
